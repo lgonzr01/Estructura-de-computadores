6d4adb0de24d00ea97b1ce94a0ace0b681e064ed
LauraGonzalez <lgonzr01@estudiantes.unileon.es>
2017-03-22 19:28:20 +0100

    Funciona hacia atras

--- ejercicioabecedario.c
+++ ejercicioabecedario.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 
+#define TAM 26
+
 int main(){
 
-	char abecedario[26];
-	int primeraLetra=98;
+	char abecedario[TAM];
+	int primeraLetra=97;
 	int numero;
+	int tamaño=0;
+	int contador=0,i,j;
 
-	for (i=0:i<length(abecedario):i++){
-		abecedario(i)=primeraLetra;
-		primeraLetra=primeraLetra++;
+	for (i=0;i<TAM;i++){
+		abecedario[i]=primeraLetra;
+		primeraLetra++;
 	}
 	do{
-		printf("Introduzca un numero entero y positivo:\n);
-		scanf("%d%,&numero);
+		printf("Introduzca un numero entero y positivo:");
+		scanf("%d",&numero);
 
-	}while(numero<=0);
+	}while(numero<=0||numero>26);
 
+	tamaño=numero*2;
+
+	for(i=0;i<numero;i++){
+		for (j=0;j<=tamaño;j++){
+			if(j==tamaño/2){
+				printf(" ");
+			}
+			if(j<=i){
+				printf("%c",abecedario[j]);
+				
+			}else if((tamaño-1-j)<=i){
+				printf("%c",abecedario[tamaño-1-j]);
+			}else{
+				printf(" ");		
+			}
+			
+		}
+		printf("\n");
+	}
+	return 0;
 }
