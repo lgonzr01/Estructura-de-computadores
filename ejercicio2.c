283169e8eb1a0dc0b5d094514011330aae8884ff
LauraGonzalez <lgonzr01@estudiantes.unileon.es>
2017-03-22 18:06:36 +0100

    Segundo ejercicio

---
+++ ejercicio2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+	char abecedario[26];
+	int primeraLetra=98;
+	int numero;
+
+	for (i=0:i<length(abecedario):i++){
+		abecedario(i)=primeraLetra;
+		primeraLetra=primeraLetra++;
+	}
+	do{
+		printf("Introduzca un numero entero y positivo:\n);
+		scanf("%d%,&numero);
+
+	}while(numero<=0);
+
+}
