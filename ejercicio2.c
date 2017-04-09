aa480fed8bab2f545a41d6884c80cc9c266b2b45
Laura Gonzalez <lgonzr01@estudiantes.unileon.es>
2017-04-09 18:00:55 +0200

    Ejercicio 2 terminado

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,19 +1,54 @@
+//LAURA GONZALEZ DEL RIO
+
 #include <stdio.h>
+
+#define TAM 26
 
 int main(){
 
-	char abecedario[26];
-	int primeraLetra=98;
+	char abecedario[TAM];
+	int primeraLetra=97;
 	int numero;
+	int tamano=0;
+	int contador=0,i,j;
 
-	for (i=0:i<length(abecedario):i++){
-		abecedario(i)=primeraLetra;
-		primeraLetra=primeraLetra++;
+	for (i=0;i<TAM;i++){
+		abecedario[i]=primeraLetra;
+		primeraLetra++;
 	}
 	do{
-		printf("Introduzca un numero entero y positivo:\n);
-		scanf("%d%,&numero);
+		printf("Introduzca un numero entero y positivo:");
+		scanf("%d",&numero);
+		if(numero<=0){
+			printf("ERROR\nIntroduzca un numero del 1-26\n");		
+		}
+	}while(numero<=0||numero>TAM);
 
-	}while(numero<=0);
+	tamano=numero*2;
 
+	for(i=0;i<numero;i++){
+		for (j=0;j<=tamano;j++){
+
+			if(j==tamano/2 && i!=(numero-1)){
+				printf(" ");
+			}
+
+			if(j==tamano/2 && i==(numero-1)){
+				printf("*");
+			}
+
+			if(j<=i){
+				printf("%c",abecedario[j]);
+				
+			}else if((tamano-1-j)<=i){
+				printf("%c",abecedario[tamano-1-j]);
+
+			}else{
+				printf(" ");		
+			}
+			
+		}
+		printf("\n");
+	}
+	return 0;
 }
